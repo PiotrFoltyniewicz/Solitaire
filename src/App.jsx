6c34b562b3bdb6fd0106146ef3577518fc36c4a0
Piotr Foltyniewicz <piotr.foltyniewicz@gmail.com>
2023-07-20 19:21:37 +0200

    game finished with moving cards by clicking

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   const [cards, setCards] = useState([[], [], [], [], [], [], [], [], [], [], [], [], []]);
   const [currentCard, setCurrentCard] = useState(null);
-  const [gameRuns, setGameRuns] = useState(() => false);
+  const [gameState, setGameState] = useState(() => 'before');
 
   function newGame() {
     setCards([[], [], [], [], [], [], [], []]);
@@ -41,7 +41,7 @@ function App() {
       return prevCards;
     })
     updateVisibility();
-    setGameRuns(true);
+    setGameState('running');
   }
 
   function showExtraCards() {
@@ -99,7 +99,7 @@ function App() {
       setCurrentCard([stackNum, cardNum]);
 
       // place card on main stacks
-    } else if (stackNum < 7) {
+    } else if (stackNum < 7 && checkPlacing(stackNum, cardNum)) {
       setCards(prevCards => {
 
         let currCard = prevCards[currentCard[0]][currentCard[1]];
@@ -122,25 +122,8 @@ function App() {
       });
       setCurrentCard(null);
 
-      // place on extra visible stack (unclick card)
-    } else if (stackNum === 8) {
-      setCards(prevCards => {
-        let currCard = prevCards[currentCard[0]][currentCard[1]];
-        const copiedCards = [...prevCards];
-
-        for (let stack of copiedCards) {
-          for (let i = 0; i < stack.length; i++) {
-            if (stack[i].id === currCard.id) {
-              stack[i].isCurrent = false;
-            }
-          }
-        }
-        return copiedCards;
-      });
-      setCurrentCard(null);
-
       // place on foundation stacks
-    } else if (stackNum > 8 && currentCard[1] === cards[currentCard[0]].length - 1) {
+    } else if (stackNum > 8 && currentCard[1] === cards[currentCard[0]].length - 1 && checkPlacing(stackNum, cardNum)) {
 
       setCards(prevCards => {
 
@@ -165,57 +148,113 @@ function App() {
       setCurrentCard(null);
     }
     else {
-      setCards(prevCards => {
-
-        let currCard = prevCards[currentCard[0]][currentCard[1]];
-        const copiedCards = [...prevCards];
-
-        for (let stack of copiedCards) {
-          let nextCards = false;
-          for (let i = 0; i < stack.length; i++) {
-            if (stack[i].id === currCard.id) {
-              nextCards = true;
-            }
-            if (nextCards) {
-              stack[i].isCurrent = false;
-            }
-          }
-        }
-
-        return copiedCards;
-      });
-      setCurrentCard(null);
+      resetClick(stackNum, cardNum);
     }
     updateVisibility();
+    gameFinished();
+  }
+
+  function checkPlacing(stackNum, cardNum) {
+
+    const currCard = cards[currentCard[0]][currentCard[1]];
+    const cardBelow = cards[stackNum][cardNum];
+
+    if (cardBelow === undefined && stackNum > 8) {
+      // placing card on empty foundation stack
+      if (currCard.number == 1) return true;
+      return false;
+    } else if (stackNum > 8) {
+      // placing card on not empty foundation stack
+      if (cardBelow.color == currCard.color && cardBelow.number + 1 == currCard.number) return true;
+      return false;
+    } else if (!cardBelow && stackNum < 7) {
+      // placing card on empty main stack
+      if (currCard.number == 13) return true;
+      return false;
+    } else if (stackNum < 7 && (currCard.color === 'spades' || currCard.color === 'clubs')) {
+      // placing card on not empty main stack when current card is red
+      if ((cardBelow.color === 'hearts' || cardBelow.color === 'diamonds') && cardBelow.number - 1 == currCard.number) return true;
+      return false;
+    } else if (stackNum < 7 && (currCard.color === 'hearts' || currCard.color === 'diamonds')) {
+      // placing card on not empty main stack when current card is red
+      if ((cardBelow.color === 'clubs' || cardBelow.color === 'spades') && cardBelow.number - 1 == currCard.number) return true;
+      return false;
+    }
+  }
+
+  function gameFinished() {
+    if (cards[9].length === 13 && cards[10].length === 13 && cards[11].length === 13 && cards[12].length === 13) {
+      setGameState('finished');
+    }
+  }
+
+  function resetClick(stackNum, cardNum) {
+    setCards(prevCards => {
+
+      let currCard = prevCards[currentCard[0]][currentCard[1]];
+      const copiedCards = [...prevCards];
+
+      for (let stack of copiedCards) {
+        let nextCards = false;
+        for (let i = 0; i < stack.length; i++) {
+          if (stack[i].id === currCard.id) {
+            nextCards = true;
+          }
+          if (nextCards) {
+            stack[i].isCurrent = false;
+          }
+        }
+      }
+
+      return copiedCards;
+    });
+    setCurrentCard(null);
+  }
+
+  const mainBoard = (
+    <>
+      <div className='upperPart gameArea'>
+        <div className='extraBoard'>
+          <ExtraStack cards={cards[7]} shownCards={cards[8]} showExtraCards={showExtraCards} stackNum={8} interact={clickCard} />
+        </div>
+        <div className='foundationBoard'>
+          <FoundationStack cards={cards[9]} stackNum={9} interact={clickCard} />
+          <FoundationStack cards={cards[10]} stackNum={10} interact={clickCard} />
+          <FoundationStack cards={cards[11]} stackNum={11} interact={clickCard} />
+          <FoundationStack cards={cards[12]} stackNum={12} interact={clickCard} />
+        </div>
+      </div>
+      <div className='mainBoard'>
+        <MainStack cards={cards[0]} stackNum={0} interact={clickCard} />
+        <MainStack cards={cards[1]} stackNum={1} interact={clickCard} />
+        <MainStack cards={cards[2]} stackNum={2} interact={clickCard} />
+        <MainStack cards={cards[3]} stackNum={3} interact={clickCard} />
+        <MainStack cards={cards[4]} stackNum={4} interact={clickCard} />
+        <MainStack cards={cards[5]} stackNum={5} interact={clickCard} />
+        <MainStack cards={cards[6]} stackNum={6} interact={clickCard} />
+      </div>
+    </>)
+
+  const gameEnd = (
+    <>
+      <div className='gameEnd'><h1>Game beaten, congratulations </h1></div>
+    </>
+  )
+  let mainGame = <></>;
+
+  switch (gameState) {
+    case 'running':
+      mainGame = mainBoard;
+      break;
+    case 'finished':
+      mainGame = gameEnd;
+      break;
   }
 
   return (
     <div>
       <Menu handleClick={newGame} />
-      {gameRuns &&
-        <>
-          <div className='upperPart gameArea'>
-            <div className='extraBoard'>
-              <ExtraStack cards={cards[7]} shownCards={cards[8]} showExtraCards={showExtraCards} stackNum={8} interact={clickCard} />
-            </div>
-            <div className='foundationBoard'>
-              <FoundationStack cards={cards[9]} stackNum={9} interact={clickCard} />
-              <FoundationStack cards={cards[10]} stackNum={10} interact={clickCard} />
-              <FoundationStack cards={cards[11]} stackNum={11} interact={clickCard} />
-              <FoundationStack cards={cards[12]} stackNum={12} interact={clickCard} />
-            </div>
-          </div>
-          <div className='mainBoard'>
-            <MainStack cards={cards[0]} stackNum={0} interact={clickCard} />
-            <MainStack cards={cards[1]} stackNum={1} interact={clickCard} />
-            <MainStack cards={cards[2]} stackNum={2} interact={clickCard} />
-            <MainStack cards={cards[3]} stackNum={3} interact={clickCard} />
-            <MainStack cards={cards[4]} stackNum={4} interact={clickCard} />
-            <MainStack cards={cards[5]} stackNum={5} interact={clickCard} />
-            <MainStack cards={cards[6]} stackNum={6} interact={clickCard} />
-          </div>
-        </>
-      }
+      {mainGame}
     </div>
 
   )
